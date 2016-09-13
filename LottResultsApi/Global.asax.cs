b1fbeb7b763b616565f5d994bb758937e85dfac5
gavin wu <gavin@ciwic.com>
2016-09-13 23:14:39 +1000

    finished data retriving through WebApi

--- LottResultsApi/Global.asax.cs
+++ LottResultsApi/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Newtonsoft.Json;
 
 namespace LottResultsApi
 {
@@ -11,6 +12,11 @@ namespace LottResultsApi
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();    GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+               ReferenceLoopHandling.Ignore;
+
+            AutofacBoot.Init();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }

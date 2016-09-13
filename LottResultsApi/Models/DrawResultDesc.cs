61223ce1764da3eee783872266f77704fea0b9b5
gavin wu <gavin@ciwic.com>
2016-09-13 16:55:28 +1000

    Finished models draft design

---
+++ LottResultsApi/Models/DrawResultDesc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LottResultsApi.Models
+{
+    public class DrawResultDesc
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int AvailableCol { get; set; }
+        public string ShowType { get; set; }
+
+        public string ColName0 { get; set; }
+        public string ColName1 { get; set; }
+        public string ColName2 { get; set; }
+        public string ColName3 { get; set; }
+        public string ColName4 { get; set; }
+        public string ColName5 { get; set; }
+        public string ColName6 { get; set; }
+        public string ColName7 { get; set; }
+        public string ColName8 { get; set; }
+        public string ColName9 { get; set; }
+
+        public ICollection<DrawResult> DrawResults { get; set; }
+    }
+}
---
+++ LottResultsApi/Models/DrawResultDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LottResultsApi.Models
+{
+    public class DrawResultDetail
+    {
+        public int Id { get; set; }
+
+        public int DrawResultId { get; set; }
+        public virtual DrawResultDesc DrawResult { get; set; }
+
+        public string ColValue0 { get; set; }
+        public string ColValue1 { get; set; }
+        public string ColValue2 { get; set; }
+        public string ColValue3 { get; set; }
+        public string ColValue4 { get; set; }
+        public string ColValue5 { get; set; }
+        public string ColValue6 { get; set; }
+        public string ColValue7 { get; set; }
+        public string ColValue8 { get; set; }
+        public string ColValue9 { get; set; }
+
+    }
+}
